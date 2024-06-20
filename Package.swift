32aae9ca38e63944b28d4a85e878d03d7f34cb54
Nakul Singh <nakul.singh@chargepoint.com>
2024-06-20 19:12:36 +0530

    added watchOS support platform in Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ let package = Package(
     platforms: [
         .macOS(.v10_10),
         .iOS(.v13),
-        .tvOS(.v13)
+        .tvOS(.v13),
+        .watchOS(.v4)
     ],
     products: [
         .library(
